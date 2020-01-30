9e18491f376f78055aa9d7d7d674b18be1b8e840
Kamil Dobrogost <kamil.dobrogost@gmail.com>
2020-01-30 20:35:07 +0100

    Add project

---
+++ Assets/Scripts/CamerMove.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamerMove : MonoBehaviour
+{
+    public int side;
+    public int enter;
+    public Transform camera;
+    public float movementSpeed;
+
+    void OnMouseEnter()
+    {
+        enter = side;
+        Debug.Log("Enter");
+    }
+
+    
+    void OnMouseExit()
+    {
+        enter = 0;
+        Debug.Log("Exit");
+    }
+   
+
+    private void Update()
+    {
+        switch (enter)
+        {
+            case 0:
+                break;
+            case 1:
+                camera.position = camera.position + new Vector3(0, 1 * movementSpeed * Time.deltaTime, 0);
+                break;
+            case 2:
+                camera.position = camera.position + new Vector3(0, -1 * movementSpeed * Time.deltaTime, 0);
+                break;
+            case 3:
+                camera.position = camera.position + new Vector3(-1 * movementSpeed * Time.deltaTime,0, 0);
+                break;
+            case 4:
+                camera.position = camera.position + new Vector3(1 * movementSpeed * Time.deltaTime, 0, 0);
+                break;
+            default:
+                break;
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/MoveWithCamera.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveWithCamera : MonoBehaviour
+{
+    public Transform camera;
+
+    void Update()
+    {
+        this.transform.position = new Vector3(camera.position.x, camera.position.y,0);
+    }
+}
