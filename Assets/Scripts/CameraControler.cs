f0e7a2af640dbe1f53b7834a600f96f97aaf472f
Kamil Dobrogost <kamil.dobrogost@gmail.com>
2020-02-03 21:11:40 +0100

    Dodanie nowej mapy i wprowadzania podstawowych mechanik

---
+++ Assets/Scripts/CameraControler.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class CameraControler : MonoBehaviour
+{
+    public float panSpeed;
+    private Vector3 pos;
+    void Update()
+    {
+        pos = transform.position;
+
+        if (Input.GetKey("w"))
+        {
+            pos.y = panSpeed * Time.deltaTime;
+        }
+
+        transform.position = pos;
+    }
+}
